97deca063772a1fd649b241ef7ddb58a6987fd1c
KKDR1998 <77356024+KKDR1998@users.noreply.github.com>
2021-02-16 10:17:29 -1000

    Completed Parts 1-8
    Need to complete part 9

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+var str = "Hello, playground"
+
+
+// Part 3:
+
+var fruitNames = [String]()
+
+// Part 4:
+
+fruitNames = ["Kiwi", "Apples", "Watermelon"]
+
+//Part 5
+print(fruitNames)
+
+//Part 6
+//"I like to eat Kiwi"
+//"I like to eat Apples"
+//"I like to eat Watermelon"
+
+print("I like to eat, \(fruitNames)")
+
+//Part 7
+//"I like to eat Kiwi"
+//"I like to eat Apples"
+//"I like to eat Watermelon"
+
+for name in fruitNames {
+    
+    
+    
+    //code block fo the for-in loop
+    print("I like to eat " + name)
+    
+    
+}
+
+// Part 8 Introduction to array indexes
+//fruitNames = ["Kiwi", "Apples", "Watermelon"]
+print(fruitNames[1])
+
+
+//part 9
+
+var roomVroom = [String]()
+
+roomVroom = []
+
+
+
+
